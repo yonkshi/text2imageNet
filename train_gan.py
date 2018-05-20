17f00c3df2f62d974b63ca82afcb669f96f4c536
Alex Hermansson <grobgeld@kth.se>
2018-05-20 12:18:27 +0200

    vanilla version of generator

--- train_gan.py
+++ train_gan.py
@@ -8,12 +8,16 @@ import tensorflow as tf
 def main():
     z = tf.random_normal((1, 1, 1,100,))
     x = tf.ones((1, 1, 1, 128,))
+    txt = tf.random_normal((1, 1, 1, 1024,))
     conved = generator(x, z)
+    conved2 = generator2(txt, z)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         out = sess.run(conved)
-        print(out)
+        out2 = sess.run(conved2)
+        print(out.shape)
+        print(out2.shape)
 
 if __name__ == '__main__':
     main()
